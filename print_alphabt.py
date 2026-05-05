78d8f6199e569d81de1c827374e414e8f226f662
Yous77130 <13012@holbertonstudents.com>
2026-05-05 11:27:07 +0200

    move print_alphabt.py to correct directory

--- print_alphabt.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-result = ''
-for i in range(ord('a'), ord('z') + 1):
-    if chr(i) != 'q' and chr(i) != 'e':
-        result += chr(i)
-print(result)
--- python_fundamentals/control_flow/print_alphabt.py
+++ python_fundamentals/control_flow/print_alphabt.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
+result = ''
 for i in range(ord('a'), ord('z') + 1):
     if chr(i) != 'q' and chr(i) != 'e':
-        print(chr(i), end='')
-print()
+        result += chr(i)
+print(result)
