ed1cebce22f8bc9b777ed975ddeef7d344563820
Yous77130 <13012@holbertonstudents.com>
2026-05-06 11:08:30 +0200

    fix pow.py handle negative exponents

--- python_fundamentals/functions_modules/pow.py
+++ python_fundamentals/functions_modules/pow.py
@@ -3,6 +3,11 @@
 
 def pow(a, b):
     result = 1
-    for i in range(b):
-        result *= a
+    if b >= 0:
+        for i in range(b):
+            result *= a
+    else:
+        for i in range(-b):
+            result *= a
+        result = 1 / result
     return result
