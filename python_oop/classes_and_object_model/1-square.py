124a700f7082e2d6aaa4a6fd5ba59418d92d344c
Yous77130 <13012@holbertonstudents.com>
2026-05-12 10:55:59 +0200

    add file for task 2

---
+++ python_oop/classes_and_object_model/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Module defining a Square class."""
+
+
+class Square:
+    """A class that represents a square."""
+
+    def __init__(self, size):
+        """Initialize a new Square."""
+        self.__size = size
